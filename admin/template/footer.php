f1ae4b6c4440b058e8240bd068b6e8c872f0205c
KLaZ <muhabdanulikhlas0983@gmail.com>
2024-02-21 07:56:10 +0700

    refactor code agar lebih efisien dan rapi

--- admin/template/footer.php
+++ admin/template/footer.php
@@ -1,159 +1,137 @@
-   <!--main content end-->
-   <!--footer start-->
-   <footer class="site-footer">
-       <div class="text-center">
-           Sistem Informasi Penjualan Kayu dan Keramik |
-           By <a href="#" style="color:yellow;font-weight:700;" target="_blank">UD ABC Salatiga Berbasis Website</a>
-           <a href="#" class="go-top">
-               <i class="fa fa-angle-up"></i>
-           </a>
-       </div>
-   </footer>
-   <!--footer end-->
-   </section>
+<footer class="site-footer">
+    <div class="text-center">
+        <!-- Penjualan Kayu dan Keramik -->
+        Sistem Informasi Penjualan Kayu dan Keramik |
+        <!-- Oleh UD ABC Salatiga Berbasis Website -->
+        By <a href="#" style="color:yellow;font-weight:700;" target="_blank">UD ABC Salatiga Berbasis Website</a>
+        <a href="#" class="go-top">
+            <i class="fa fa-angle-up"></i>
+        </a>
+    </div>
+</footer>
 
-   <!-- js placed at the end of the document so the pages load faster -->
-   <script src="assets/js/jquery.js"></script>
-   <script src="assets/js/jquery-1.8.3.min.js"></script>
-   <script src="assets/js/bootstrap.min.js"></script>
-   <script class="include" type="text/javascript" src="assets/js/jquery.dcjqaccordion.2.7.js"></script>
-   <script src="assets/js/jquery.scrollTo.min.js"></script>
-   <script src="assets/js/jquery.nicescroll.js" type="text/javascript"></script>
-   <script src="assets/js/jquery.sparkline.js"></script>
+<!-- JavaScript -->
+<script src="assets/js/jquery.js"></script>
+<script src="assets/js/jquery-1.8.3.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+<script class="include" type="text/javascript" src="assets/js/jquery.dcjqaccordion.2.7.js"></script>
+<script src="assets/js/jquery.scrollTo.min.js"></script>
+<script src="assets/js/jquery.nicescroll.js" type="text/javascript"></script>
+<script src="assets/js/jquery.sparkline.js"></script>
+<script src="assets/datatables/jquery.dataTables.min.js"></script>
+<script src="assets/datatables/dataTables.bootstrap.min.js"></script>
+<script src="assets/js/common-scripts.js"></script>
+<script type="text/javascript" src="assets/js/gritter/js/jquery.gritter.js"></script>
+<script type="text/javascript" src="assets/js/gritter-conf.js"></script>
+<script src="assets/js/sparkline-chart.js"></script>
+<script src="assets/js/zabuto_calendar.js"></script>
 
-   <script src="assets/datatables/jquery.dataTables.min.js"></script>
-   <script src="assets/datatables/dataTables.bootstrap.min.js"></script>
+<script type="text/javascript">
+    // DataTable initialization
+    $(function () {
+        $("#example1").DataTable();
+        $('#example2').DataTable();
+    });
 
-   <!--common script for all pages-->
-   <script src="assets/js/common-scripts.js"></script>
+    // Notification untuk stok barang
+    <?php
+    $sql = "select * from barang where stok <= 3";
+    $row = $config->prepare($sql);
+    $row->execute();
+    $q = $row->fetch();
+    if ($q['stok'] <= 3) {
+    ?>
+        $(document).ready(function () {
+            var unique_id = $.gritter.add({
+                title: 'Peringatan !',
+                text: 'Stok barang tersisa kurang dari 3, silakan pesan lagi!',
+                image: 'assets/img/seru.png',
+                sticky: true,
+                time: '',
+                class_name: 'my-sticky-class'
+            });
+            return false;
+        });
+    <?php } ?>
+</script>
 
-   <script type="text/javascript" src="assets/js/gritter/js/jquery.gritter.js"></script>
-   <script type="text/javascript" src="assets/js/gritter-conf.js"></script>
+<script type="application/javascript">
+    // Calendar initialization
+    $(document).ready(function () {
+        $("#date-popover").popover({
+            html: true,
+            trigger: "manual"
+        });
+        $("#date-popover").hide();
+        $("#date-popover").click(function (e) {
+            $(this).hide();
+        });
 
-   <!--script for this page-->
-   <script src="assets/js/sparkline-chart.js"></script>
-   <script src="assets/js/zabuto_calendar.js"></script>
+        $("#my-calendar").zabuto_calendar({
+            action: function () {
+                return myDateFunction(this.id, false);
+            },
+            action_nav: function () {
+                return myNavFunction(this.id);
+            },
+            ajax: {
+                url: "",
+                modal: true
+            },
+            legend: []
+        });
+    });
 
-   <script type="text/javascript">
-       //datatable
-       $(function () {
-           $("#example1").DataTable();
-           $('#example2').DataTable();
-       });
-   </script>
-   <?php
-			$sql=" select * from barang where stok <=3";
-			$row = $config -> prepare($sql);
-			$row -> execute();
-			$q = $row -> fetch();
-				if($q['stok'] == 3){	
-				if($q['stok'] == 2){	
-				if($q['stok'] == 1){	
-		?>
-   <script type="text/javascript">
-       //template
-       $(document).ready(function () {
-           var unique_id = $.gritter.add({
-               // (string | mandatory) the heading of the notification
-               title: 'Peringatan !',
-               // (string | mandatory) the text inside the notification
-               text: 'stok barang ada yang tersisa kurang dari 3 silahkan pesan lagi !',
-               // (string | optional) the image to display on the left
-               image: 'assets/img/seru.png',
-               // (bool | optional) if you want it to fade out on its own or just sit there
-               sticky: true,
-               // (int | optional) the time you want it to be alive for before fading out
-               time: '',
-               // (string | optional) the class name you want to apply to that specific message
-               class_name: 'my-sticky-class'
+    function myNavFunction(id) {
+        $("#date-popover").hide();
+        var nav = $("#" + id).data("navigation");
+        var to = $("#" + id).data("to");
+        console.log('nav ' + nav + ' to: ' + to.month + '/' + to.year);
+    }
 
-           });
+    // Alert tampil setelah document ready
+    $(document).ready(function () {
+        setTimeout(function () {
+            $(".alert-danger").fadeIn('slow');
+        }, 500);
+    });
+    // Alert hilang setelah beberapa detik
+    setTimeout(function () {
+        $(".alert-danger").fadeOut('slow');
+    }, 5000);
 
-           return false;
-       });
-   </script>
-   <?php }}}?>
-   <script type="application/javascript">
-       $(document).ready(function () {
-           $("#date-popover").popover({
-               html: true,
-               trigger: "manual"
-           });
-           $("#date-popover").hide();
-           $("#date-popover").click(function (e) {
-               $(this).hide();
-           });
+    $(document).ready(function () {
+        setTimeout(function () {
+            $(".alert-success").fadeIn('slow');
+        }, 500);
+    });
+    setTimeout(function () {
+        $(".alert-success").fadeOut('slow');
+    }, 5000);
 
-           $("#my-calendar").zabuto_calendar({
-               action: function () {
-                   return myDateFunction(this.id, false);
-               },
-               action_nav: function () {
-                   return myNavFunction(this.id);
-               },
-               ajax: {
-                   url: "",
-                   modal: true
-               },
-               legend: []
-           });
-       });
+    $(document).ready(function () {
+        setTimeout(function () {
+            $(".alert-warning").fadeIn('slow');
+        }, 500);
+    });
+    setTimeout(function () {
+        $(".alert-success").fadeOut('slow');
+    }, 5000);
+</script>
 
+<script>
+    // Modal JavaScript
+    $(".modal-create").hide();
+    $(".bg-shadow").hide();
 
-       function myNavFunction(id) {
-           $("#date-popover").hide();
-           var nav = $("#" + id).data("navigation");
-           var to = $("#" + id).data("to");
-           console.log('nav ' + nav + ' to: ' + to.month + '/' + to.year);
-       }
+    function clickModals() {
+        $(".bg-shadow").fadeIn();
+        $(".modal-create").fadeIn();
+    }
 
-
-       //angka 500 dibawah ini artinya pesan akan muncul dalam 0,5 detik setelah document ready
-       $(document).ready(function () {
-           setTimeout(function () {
-               $(".alert-danger").fadeIn('slow');
-           }, 500);
-       });
-       //angka 3000 dibawah ini artinya pesan akan hilang dalam 3 detik setelah muncul
-       setTimeout(function () {
-           $(".alert-danger").fadeOut('slow');
-       }, 5000);
-
-       $(document).ready(function () {
-           setTimeout(function () {
-               $(".alert-success").fadeIn('slow');
-           }, 500);
-       });
-       setTimeout(function () {
-           $(".alert-success").fadeOut('slow');
-       }, 5000);
-
-       $(document).ready(function () {
-           setTimeout(function () {
-               $(".alert-warning").fadeIn('slow');
-           }, 500);
-       });
-       setTimeout(function () {
-           $(".alert-success").fadeOut('slow');
-       }, 5000);
-   </script>
-   <script>
-       $(".modal-create").hide();
-       $(".bg-shadow").hide();
-       $(".bg-shadow").hide();
-
-       function clickModals() {
-           $(".bg-shadow").fadeIn();
-           $(".modal-create").fadeIn();
-       }
-
-       function cancelModals() {
-           $('.modal-view').fadeIn();
-           $(".modal-create").hide();
-           $(".bg-shadow").hide();
-       }
-   </script>
-
-   </body>
-
-   </html>
+    function cancelModals() {
+        $('.modal-view').fadeIn();
+        $(".modal-create").hide();
+        $(".bg-shadow").hide();
+    }
+</script>
